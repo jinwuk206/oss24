b8b76ff6bd425db2cd81f317c5df93ab3857f82f
unknown <assa4677@gmail.com>
2025-11-26 10:53:53 +0900

    first commit

---
+++ lumache.py
@@ -0,0 +1,10 @@
+def get_random_ingredients(kind=None):
+    """
+    Return a list of random ingredients as strings.
+    
+    :param kind: Optional "kind" of ingredients.
+    :type kind: list[str] or None
+    :return: The ingredients list.
+    :rtype: list[str]
+    """
+    return ["shells", "gorgonzola", "parsley"]
